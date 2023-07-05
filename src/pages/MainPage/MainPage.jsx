242bd7906425d11658efd0940919d30ea6db6d24
mramory <119244930+mramory@users.noreply.github.com>
2023-07-05 20:18:49 +0300

    header v_1

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,9 @@
+import { Header } from "../../components/Header/Header"
+
+export const MainPage = () => {
+    return(
+        <div className="container">
+            <Header />
+        </div>
+    )
+}
